d06145757165fae5fa83af1419f3397ed6e4122e
Vivek Maniyar <vivekmaniyar9601@gmail.com>
2022-07-19 15:34:29 +0530

    Changed port number and added new method

--- FIRST_MICROPROFILE_APP/src/main/java/com/vivek/first_microprofile_app/service/ExampleService.java
+++ FIRST_MICROPROFILE_APP/src/main/java/com/vivek/first_microprofile_app/service/ExampleService.java
@@ -2,13 +2,24 @@ package com.vivek.first_microprofile_app.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/example")
 public class ExampleService {
 
     @GET
     public String get() {
-        return "Hello World!";
+        return "Hello From Microprofile App!";
+    }
+    
+    @GET
+    @Path("add/{x}/{y}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sum(@PathParam("x")Integer x,@PathParam("y")Integer y){
+        Integer r = x+y;
+        return r.toString();
     }
 
 }
